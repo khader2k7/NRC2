614b37b438a42c70fd5708a057f9c10762e93dfc
khader2k7 <38935050+khader2k7@users.noreply.github.com>
2018-05-03 18:32:08 +0300

    Add files via upload

--- build/mm.build.js
+++ build/mm.build.js
@@ -71954,9 +71954,9 @@ angular.module('mm.addons.mod_workshop')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.moodle.moodlemobile",
-    "appname": "Moodle Mobile",
-    "desktopappname": "Moodle Desktop",
+    "app_id" : "net.imlearning.www",
+    "appname": "NRC learning",
+    "desktopappname": "NRC learning",
     "versioncode" : "2023",
     "versionname" : "3.4.2",
     "cache_expiration_time" : 300000,
@@ -71964,9 +71964,9 @@ angular.module('mm.core')
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch", "de-du": "Deutsch - Du", "el": "Ελληνικά", "en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fi": "Suomi", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語", "ko" : "한국어", "lt" : "Lietuvių", "mr": "मराठी", "nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sr-cr": "Српски", "sr-lt": "Srpski", "sv": "Svenska", "tr" : "Türkçe", "uk" : "Українська", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"student": {"url": "https://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "https://school.demo.moodle.net", "username": "teacher", "password": "moodle"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
+	"presets" : {"url": "http://www.imlearning.net/lms", "demo": "NRCdemo1@3"},
     "siteurl": "http://www.imlearning.net/lms",
     "multisitesdisplay": "select",
     "skipssoconfirmation": "false",
